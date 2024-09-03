ef40c46309efbaadf81c37cae3ee8f53e4f71c39
dizgen <dzigen@gmail.com>
2024-09-03 23:07:56 +0300

    vectordb-connector structure

--- src/embedding_functions.py
+++ src/embedding_functions.py
@@ -1,5 +1,11 @@
+from abc import ABC, abstractmethod
+import chromadb
 
-
+# логироавние 
+# обработка исключений
+# тестирование
+# восстановление соединения
+ 
 
 class EmbeddingDatabaseConnection:
     def __init__(self, some_other_params = None):
@@ -21,3 +27,73 @@ class EmbeddingDatabaseConnection:
     def get_triplet_emb(self, some_other_params = None):
         # TODO
         pass
+
+class DatabaseInterface(ABC):
+    
+    @abstractmethod
+    def open_connection(self):
+        # открытие соединения с бд
+        pass
+
+    @abstractmethod
+    def close_connection(self):
+        # закрытие соединения с бд
+        pass
+
+    @abstractmethod
+    def create(self):
+        # добавить вектора/метаданные/документы/идентификаторы
+        pass
+
+    @abstractmethod
+    def delete(self):
+        # удалить елементы по идентификатору
+        pass
+
+    @abstractmethod
+    def update(self):
+        # обновить документ/метаданные для конкретного семпла
+        pass
+
+    @abstractmethod
+    def read(self):
+        # получить элемент по идентификатору 
+        pass
+
+    def __del__(self):
+        self.close_connection()
+
+
+class ChromaConnection(DatabaseInterface):
+
+    def __init__(self) -> None:
+        pass
+        
+    def open_connection(self):
+        pass
+
+    def close_connection(self):
+        pass
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
+
+class EmbedderModel:
+    def __init__(self) -> None:
+        pass
+
+    def encode_query(self):
+        pass
+
+    def encode_document(self):
+        pass
+        
