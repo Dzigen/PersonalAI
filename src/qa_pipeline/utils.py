759daf76140afb19f76453756a719483b3fe1a97
dizgen <dzigen@gmail.com>
2024-09-10 04:41:38 +0300

    add default-values for all fields of qa-pipeline config

--- src/qa_pipeline/utils.py
+++ src/qa_pipeline/utils.py
@@ -7,7 +7,7 @@ from .answer_generator import QALLMGeneratorConfig
 
 @dataclass
 class QAPipelineConfig:
-    knowledge_retriever_config: KnowledgeRetrieverConfig
+    knowledge_retriever_config: KnowledgeRetrieverConfig = field(default_factory=lambda: KnowledgeRetrieverConfig())
     query_parser_config: QueryLLMParserConfig = field(default_factory=lambda: QueryLLMParserConfig())
     knowledge_comparator_config: KnowledgeComparatorConfig = field(default_factory=lambda: KnowledgeComparatorConfig())
     answer_generator_config: QALLMGeneratorConfig = field(default_factory=lambda: QALLMGeneratorConfig())
