168f09e0d70f5543e87c2ea2bfccd5c7e83e2bea
dizgen <dzigen@gmail.com>
2024-10-08 17:06:03 +0300

    add in-memory cache in available pull

--- src/qa_pipeline/knowledge_retriever/cache/configs.py
+++ src/qa_pipeline/knowledge_retriever/cache/configs.py
@@ -1,13 +1,16 @@
 from dataclasses import field
 
 from .AerospikeConnector import AerospikeConnector, DEFAULT_AEROSPIKE_CONFIG
+from .InMemoryConnector import InMemoryConnector, DEFAULT_INMEMORY_CONFIG
 
 DEFAULT_KVDB_CONFIGS = {
-    'aerospike': DEFAULT_AEROSPIKE_CONFIG
+    'aerospike': DEFAULT_AEROSPIKE_CONFIG,
+    'inmemory': DEFAULT_INMEMORY_CONFIG
 }
 
 AVAILABLE_KVDB_CONNECTORS = {
-    'aerospike': AerospikeConnector
+    'aerospike': AerospikeConnector,
+    'inmemory': InMemoryConnector
 }
 
 # TODO
