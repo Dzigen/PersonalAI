a307f917052e7d611c0c9bdb29944730ce7fc7ae
dizgen <dzigen@gmail.com>
2024-10-09 17:19:11 +0300

    quick fix

--- src/qa_pipeline/knowledge_retriever/TripletsFilter.py
+++ src/qa_pipeline/knowledge_retriever/TripletsFilter.py
@@ -31,8 +31,8 @@ class TripletsFilter(AbstractTriplesFilter):
         #self.log(f"triple ids: {base_triplets_ids}", verbose=self.log_verbose)
 
         if len(base_triplets_ids) > 0:
-            print(base_triplets_ids)
-            print(query_instance)
+            #print(base_triplets_ids)
+            #print(query_instance)
             raw_relevant_triplets = self.kg_model.embeddings_db.vectordbs['triplets'].retrieve(
                 [query_instance], self.config.max_k, includes=['embeddings', 'documents', 'metadatas'], where={"id": {"$in": base_triplets_ids}})[0]
             accepted_tripletes_ids = list(map(lambda item: item[1].id, raw_relevant_triplets))
