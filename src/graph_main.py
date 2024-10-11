cbd38972c3765a1b5dbeeb6e6840e3a4baaaebca
dizgen <dzigen@gmail.com>
2024-10-12 01:14:14 +0300

    clean src folder + add logg for RemoteKnowledgeGraph-class

--- src/graph_main.py
+++ src/graph_main.py
@@ -2,20 +2,19 @@ import ast
 from dataclasses import dataclass, field
 from typing import List
 
-from .db_models.graph_db.neo4j_functions import Neo4jConnection, Neo4jConnectionConfig
-from .agents.private import GigaChatAgent
-from .utils import Logger
+from .knowledge_graph_model import GraphModel, GraphModelConfig, EmbeddingsModelConfig, EmbeddingsModel
 from .qa_pipeline import QAPipeline, QAPipelineConfig
 from .memorize_pipeline import MemPipeline, MemPipelineConfig
 from .knowledge_graph_model import KnowledgeGraphModel
-from .db_models.embeddings_db.embedding_functions import EmbeddingsDatabaseConnection, EmbeddingsDatabaseConnectionConfig
+from .agents.private import GigaChatAgent
+from .utils import Logger
 
 RKG_LOG_PATH = "rmkg_log"
 
 @dataclass
 class RemoteKnowledgeGraphConfig:
-    graph_db_config: Neo4jConnectionConfig
-    embedder_db_config: EmbeddingsDatabaseConnectionConfig
+    graph_sturct_config: GraphModelConfig = field(default_factory=lambda:GraphModelConfig())
+    embedds_struct_config: EmbeddingsModelConfig = field(default_factory=lambda: EmbeddingsModelConfig())
     qa_pipeline_config: QAPipelineConfig = field(default_factory=lambda:QAPipelineConfig())
     mem_pipeline_config: MemPipelineConfig = field(default_factory=lambda:MemPipelineConfig())
     log: Logger = field(default_factory=Logger(RKG_LOG_PATH))
@@ -27,18 +26,25 @@ class RemoteKnowledgeGraph:
         self.log = self.config.log
         
         self.kg_model = KnowledgeGraphModel(
-            graph_db=Neo4jConnection(config.graph_db_config),
-            embeddings_db=EmbeddingsDatabaseConnection(config.embedder_db_config))
+            graph_struct=GraphModel(config.graph_struct_config),
+            embeddings_struct=EmbeddingsModel(config.embedds_struct_config))
         self.llm_agent = GigaChatAgent()
         self.qa_pipeline = QAPipeline(kg_model=self.kg_model, llm_agent=self.llm_agent, config=config.qa_pipeline_config)
         self.mem_pipeline = MemPipeline(agent_conn=self.llm_agent, kg_model=self.kg_model, config=config.mem_pipeline_config)
 
     def answer_question(self, question: str) -> str:
-        return self.qa_pipeline.answer(question)
+        self.log("Start answer generation:", verbose=self.config.log_verbose)
+        self.log(f"- question: {question}")
+        answer = self.qa_pipeline.answer(question)
+        self.log(f"- answer: {answer}")
+        return answer
 
     def update_memory(self, new_info: List[str]):
+        self.log("Start memory-updating...", verbose=self.config.log_verbose)
         for info in new_info:
             self.mem_pipeline.remember(info)
+        self.log("Memory updated successfully!", verbose=self.config.log_verbose)
+            
         
                 
         
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,3 @@
 from .evaluation_metrics import ReaderMetrics
-from .old_utils import Logger
+from .utils import Logger
 from .data_structs import Triplet, TripletCreator, NodeCreator
