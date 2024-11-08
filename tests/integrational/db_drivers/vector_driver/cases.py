60ae72975ac7606fb8c82d4de38ec965a05a0b08
dizgen <dzigen@gmail.com>
2024-11-08 13:18:06 +0300

    add test-cases for vector-driver

--- tests/integrational/db_drivers/vector_driver/cases.py
+++ tests/integrational/db_drivers/vector_driver/cases.py
@@ -1,4 +1,6 @@
 from chromadb.errors import ChromaError
+import torch
+import numpy
 
 import sys
 sys.path.insert(0, "../../../")
@@ -10,6 +12,17 @@ FULL_INSTANCE2 = VectorDBInstance(id='456', document='ytrewq', embedding=[0.4,0.
 INSTANCE_WO_METADATA = VectorDBInstance(id='456', document='ytrewq', embedding=[0.4,0.5,0.6])
 INSTANCE_WO_ID = VectorDBInstance(document='ytrewq', embedding=[0.4,0.5,0.6])
 INSTANCE_WO_EMBEDDING = VectorDBInstance(id='456', document='ytrewq')
+
+INSTANCE_WITH_BAD_ID1 = VectorDBInstance(id=123, document='qwerty', embedding=[0.1,0.2,0.3])
+INSTANCE_WITH_BAD_ID2 = VectorDBInstance(id=True, document='qwerty', embedding=[0.1,0.2,0.3])
+INSTANCE_WITH_BAD_ID3 = VectorDBInstance(id=None, document='qwerty', embedding=[0.1,0.2,0.3])
+
+INSTANCE_WITH_TORCH_EMB = VectorDBInstance(id='123', document='qwerty', embedding=torch.tensor([0.1,0.2,0.3]), metadata={'k1': 'v1'})
+INSTANCE_WITH_NUMPY_EMB = VectorDBInstance(id='123', document='qwerty', embedding=numpy.array([0.1,0.2,0.3]), metadata={'k1': 'v1'})
+
+INSTANCE_WITH_BAD_EMB1 = VectorDBInstance(id='456', document='ytrewq', embedding="[0.4,0.5,0.6]")
+INSTANCE_WITH_BAD_EMB2 = VectorDBInstance(id='456', document='ytrewq', embedding=None)
+INSTANCE_WITH_BAD_EMB3 = VectorDBInstance(id='456', document='ytrewq', embedding=[[0.4,0.5,0.6]])
 
 VECTORDB_CREAT_TEST_CASES = [
     # пустой список
@@ -29,53 +42,97 @@ VECTORDB_CREAT_TEST_CASES = [
     # элемент существует в бд (по id)
     ([[FULL_INSTANCE1],[FULL_INSTANCE1]], {'exception': False, 'db_size': 1}),
     # неверный формат идентикатора 1
-    () # TODO
+    ([[INSTANCE_WITH_BAD_ID1]],{'exception': True, 'db_size': 0}),
     # неверный формат идентификатора 2
-    () # TODO
+    ([[INSTANCE_WITH_BAD_ID2]],{'exception': True, 'db_size': 0}),
+    # неверный формат идентификатора 3
+    ([[INSTANCE_WITH_BAD_ID3]],{'exception': True, 'db_size': 0})
     ]
 
 VECTORDB_DELETE_TEST_CASES = [
     # пустой список
-    ()
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [], {'exception': False, 'db_size': 2})
     # удаление одного существующего элемента
-    (),
+    ([FULL_INSTANCE1,FULL_INSTANCE2], ['456'], {'exception': False, 'db_size': 1}),
     # удаление одного несуществующего элемента
+    ([FULL_INSTANCE1,FULL_INSTANCE2], ['789'], {'exception': False, 'db_size': 2}),
     # в списке элементов на удаление есть несуществующие
+    ([FULL_INSTANCE1,FULL_INSTANCE2], ['456','789'], {'exception': False, 'db_size': 1}),
     # в списке элементов на удаление все существуют
+    ([FULL_INSTANCE1,FULL_INSTANCE2], ['123','456'], {'exception': False, 'db_size': 0}),
     #неверный формат идентификаторов 1
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [123], {'exception': True, 'db_size': 2}),
     # неверный формат идентификатора 2
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [True], {'exception': True, 'db_size': 2}),
+    # неверный формат идентификатора 3
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [None], {'exception': True, 'db_size': 2}),
 ]
 
 VECTORDB_READ_TEST_CASES = [
     # пустой список
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], [], {'exception': False, 'output_ids': []}),
     # один существующий элемент
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], ['123'], {'exception': False, 'output_ids': ['123']}),
     # один несуществующий элемент
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], ['789'], {'exception': False, 'output_ids': [None]}),
     # несколько существующих элементов
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], ['123', '456'], {'exception': False, 'output_ids': ['123','456']}),
     # в списке есть несуществующий элемент
-    # неверный формат идентификатора 1
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], ['123', '789'], {'exception': False, 'output_ids': ['123',None]})
+    #неверный формат идентификаторов 1
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], [123], {'exception': True, 'output_ids': []}),
     # неверный формат идентификатора 2
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], [True], {'exception': True, 'output_ids': []}),
+    # неверный формат идентификатора 3
+    ([[FULL_INSTANCE1,FULL_INSTANCE2]], [None], {'exception': True, 'output_ids': []}),
 ]
 
 VECTORDB_RETRIEVE_TEST_CASES = [
     # ретрив по одному квери
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [FULL_INSTANCE1], 1, {'exception': False, 'output_size': [1]}),
     # ретрив по нескольким квери
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [FULL_INSTANCE1, FULL_INSTANCE1], 1, {'exception': False, 'output_size': [1, 1]}),
     # в бд меньше элементов, чем заданное количество
-    # в бд больше элементов, чем заданное количество
+    ([FULL_INSTANCE1], [FULL_INSTANCE1], 2, {'exception': False, 'output_size': [1]}),
+    # torch-тип данных эмбеддинга
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [INSTANCE_WITH_TORCH_EMB], 2, {'exception': False, 'output_size': [1]}),
+    # numpy-тип данных эмбеддинга
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [INSTANCE_WITH_NUMPY_EMB], 2, {'exception': False, 'output_size': [1]}),
+    # неверный формат ембеддинга квери # 1
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [INSTANCE_WITH_BAD_EMB1], 2, {'exception': True, 'output_size': [-1]}),
+    # неверный формат ембеддинга квери # 2
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [INSTANCE_WITH_BAD_EMB2], 2, {'exception': True, 'output_size': [-1]}),
+    # неверный формат ебмеддинга квери # 3
+    ([FULL_INSTANCE1,FULL_INSTANCE2], [INSTANCE_WITH_BAD_EMB2], 2, {'exception': True, 'output_size': [-1]}),
 ]
 
 VECTORDV_COUNT_TEST_CASES = [
     # нуль элементов
+    ([], 0),
     # один Элемент
+    ([FULL_INSTANCE1], 1),
     # несколько элементов
+    ([FULL_INSTANCE1,FULL_INSTANCE2], 2),
 ]
 
 VECTORDB_EXIST_TEST_CASES = [
     # элемент существует
+    ([FULL_INSTANCE1,FULL_INSTANCE2], '123', {'exception': False, 'exist': True})
     # элемента не существует
+    ([FULL_INSTANCE1,FULL_INSTANCE2], '789', {'exception': False, 'exist': False})
+    # неверный формат идентификатора # 1
+    ([FULL_INSTANCE1,FULL_INSTANCE2], 789, {'exception': True, 'exist': False})
+    # неверный формат идентификатора # 2
+    ([FULL_INSTANCE1,FULL_INSTANCE2], False, {'exception': True, 'exist': False})
+    # неверный формат идентификатора # 3
+    ([FULL_INSTANCE1,FULL_INSTANCE2], None, {'exception': True, 'exist': False})
 ]
 
 VECTORDB_CLEAR_TEST_CASES = [
     # чистка пустой бд
+    [],
     # чиста бд с одним элементов
+    [FULL_INSTANCE1],
     # чиста бд с несколькими элементами
+    [FULL_INSTANCE1,FULL_INSTANCE2]
 ]
--- tests/integrational/db_drivers/vector_driver/test_chroma.py
+++ tests/integrational/db_drivers/vector_driver/test_chroma.py
@@ -11,8 +11,8 @@ from cases import VECTORDB_CREAT_TEST_CASES, VECTORDB_DELETE_TEST_CASES, \
 
 @pytest.mark.parametrize("input, expected", VECTORDB_CREAT_TEST_CASES)
 def test_create(input, expected, chromadb_conn):
-
     chromadb_conn.clear()
+
     try:
         for inp in input:
             chromadb_conn.create(inp)
@@ -23,24 +23,58 @@ def test_create(input, expected, chromadb_conn):
 
 @pytest.mark.parametrize("instances, input, expected", VECTORDB_DELETE_TEST_CASES)
 def test_delete(instances, input, expected, chromadb_conn):
-    pass
+    chromadb_conn.clear()
+    chromadb_conn.create(instances)
+
+    try:
+        chromadb_conn.delete(input)
+    except Exception as e:
+        assert expected['exception']
+
+    assert chromadb_conn.count_items() == expected['db_size']
 
 @pytest.mark.parametrize("input, expected", VECTORDB_READ_TEST_CASES)
 def test_read(instances, input, expected, chromadb_conn):
-    pass
+    chromadb_conn.clear()
+    chromadb_conn.create(instances)
+
+    try:
+        output = chromadb_conn.read(input)
+    except Exception as e:
+        assert expected['exception']
+
+    if expected['exception']:
+        assert list(map(lambda item: item.id, output)) == expected['output_ids']
 
 @pytest.mark.parametrize("input, expected", VECTORDB_RETRIEVE_TEST_CASES)
-def test_retrieve(instances, input, expected, chromadb_conn):
-    pass
+def test_retrieve(instances, queries, n_results, expected, chromadb_conn):
+    chromadb_conn.clear()
 
 @pytest.mark.parametrize("input, expected", VECTORDV_COUNT_TEST_CASES)
-def test_count_items(input, expected, chromadb_conn):
-    pass
+def test_count_items(instances, expected, chromadb_conn):
+    chromadb_conn.clear()
+    chromadb_conn.create(instances)
 
-@pytest.mark.parametrize("input, expected", VECTORDB_EXIST_TEST_CASES)
+    assert chromadb_conn.count_items() == expected
+
+@pytest.mark.parametrize("instances, input, expected", VECTORDB_EXIST_TEST_CASES)
 def test_item_exist(instances, input, expected, chromadb_conn):
-    pass
+    chromadb_conn.clear()
+    chromadb_conn.create(instances)
 
-@pytest.mark.parametrize("input, expected", VECTORDB_CLEAR_TEST_CASES)
-def test_clear(instances, expected, chromadb_conn):
-    pass
+    try:
+        real = chromadb_conn.item_exist(input)
+    except Exception as e:
+        assert expected['exception']
+
+    if expected['exception']:
+        assert real == expected['exist']
+
+@pytest.mark.parametrize("instances", VECTORDB_CLEAR_TEST_CASES)
+def test_clear(instances, chromadb_conn):
+    chromadb_conn.clear()
+    chromadb_conn.create(instances)
+
+    assert chromadb_conn.count_items() == len(instances)
+    chromadb_conn.clear()
+    assert chromadb_conn.count_items() == 0
