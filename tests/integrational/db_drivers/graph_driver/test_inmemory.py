653c0f59cdc05a44baf913e7fddc7cc7e1f1152f
dizgen <dzigen@gmail.com>
2024-11-08 04:51:07 +0300

    quick fix

--- tests/integrational/db_drivers/graph_driver/test_inmemory.py
+++ tests/integrational/db_drivers/graph_driver/test_inmemory.py
@@ -4,35 +4,35 @@ from chromadb.errors import ChromaError
 import sys
 sys.path.insert(0, "../../")
 
-from cases import KVDB_CREAT_TEST_CASES, KVDB_DELETE_TEST_CASES, \
-    KVDB_READ_TEST_CASES, KVDB_COUNT_TEST_CASES, KVDB_EXIST_TEST_CASES, \
-    KVDB_CLEAR_TEST_CASES
+from cases import GRAPHDB_CREAT_TEST_CASES, GRAPHDB_DELETE_TEST_CASES, \
+    GRAPHDB_READ_TEST_CASES, GRAPHDB_COUNT_TEST_CASES, GRAPHDB_EXIST_TEST_CASES, \
+    GRAPHDB_CLEAR_TEST_CASES
 
-@pytest.mark.parametrize("input, expected", KVDB_CREAT_TEST_CASES)
+@pytest.mark.parametrize("input, expected", GRAPHDB_CREAT_TEST_CASES)
 def test_create(input, expected, inmemory_conn):
     pass
 
 
-@pytest.mark.parametrize("input, expected", KVDB_DELETE_TEST_CASES)
+@pytest.mark.parametrize("input, expected", GRAPHDB_DELETE_TEST_CASES)
 def test_delete(input, expected, inmemory_conn):
     pass
 
 
-@pytest.mark.parametrize("input, expected", KVDB_READ_TEST_CASES)
+@pytest.mark.parametrize("input, expected", GRAPHDB_READ_TEST_CASES)
 def test_read(input, expected, inmemory_conn):
     pass
 
 
-@pytest.mark.parametrize("input, expected", KVDB_COUNT_TEST_CASES)
+@pytest.mark.parametrize("input, expected", GRAPHDB_COUNT_TEST_CASES)
 def test_count(input, expected, inmemory_conn):
     pass
 
 
-@pytest.mark.parametrize("input, expected", KVDB_EXIST_TEST_CASES)
+@pytest.mark.parametrize("input, expected", GRAPHDB_EXIST_TEST_CASES)
 def test_exist(input, expected, inmemory_conn):
     pass
 
 
-@pytest.mark.parametrize("input, expected", KVDB_CLEAR_TEST_CASES)
+@pytest.mark.parametrize("input, expected", GRAPHDB_CLEAR_TEST_CASES)
 def test_clear(input, expected, inmemory_conn):
     pass
