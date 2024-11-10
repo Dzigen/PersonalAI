f90b4acf15eb8ff4b24284826138ece3299cb9b8
dizgen <dzigen@gmail.com>
2024-11-11 02:06:37 +0300

    quick fix

--- tests/integrational/db_drivers/vector_driver/conftest.py
+++ tests/integrational/db_drivers/vector_driver/conftest.py
@@ -7,5 +7,5 @@ from src.db_drivers.vector_driver import VectorDriver, VectorDriverConfig, Vecto
 @pytest.fixture()
 def chromadb_conn():
     config = VectorDriverConfig(db_vendor='chroma', db_config=VectorDBConnectionConfig(
-            path="./volumes/chroma", db_name='testing', need_to_clear=True))
+            path="./volumes/chroma", db_info={'db':'testing', 'table': 'testing'}, need_to_clear=True))
     return VectorDriver.connect(config)
