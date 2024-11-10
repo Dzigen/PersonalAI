8bd6fb326eb81d8c6b1b3d72218289a11e975c90
dizgen <dzigen@gmail.com>
2024-11-10 23:43:48 +0300

    add test assertion for create/delete-methods of embeddings-model and create-method of graph-model

--- tests/integrational/kg_model/test_embeddings_model.py
+++ tests/integrational/kg_model/test_embeddings_model.py
@@ -6,12 +6,44 @@ import pytest
 
 from cases import EM_CREATE_TESTS, EM_DELETE_TESTS
 
-@pytest.mark.parametrize("triplets, expected", EM_CREATE_TESTS)
-def test_create_triplets(embeddings_model):
-    # TODO
-    pass
+@pytest.mark.parametrize("triplets, add_nodes_flag, expected", EM_CREATE_TESTS)
+def test_create_triplets(triplets, add_nodes_flag, expected, embeddings_model):
+    embeddings_model.vectordbs['nodes'].clear()
+    embeddings_model.vectordbs['triplets'].clear()
 
-@pytest.mark.parametrize("triplets, expected", EM_DELETE_TESTS)
-def test_delete_triplets(embeddings_model):
-    # TODO
-    pass
+    created_item_ids = embeddings_model.create_triplets(triplets, add_nodes=add_nodes_flag, batch_size=128)
+
+    real_nodes_db_size = embeddings_model.vectordbs['nodes'].count_items()
+    real_triplets_db_size = embeddings_model.vectordbs['triplets'].count_items()
+    assert real_nodes_db_size == expected['nodes_count']
+    assert real_triplets_db_size == expected['triplets_count']
+    assert real_nodes_db_size == len(created_item_ids['nodes'])
+    assert real_triplets_db_size == len(created_item_ids['triplets'])
+
+    if add_nodes_flag:
+        for node_id in created_item_ids['nodes']:
+            assert embeddings_model.vectordbs['nodes'].item_exist(node_id)
+    for triplet_id in created_item_ids['triplets']:
+        assert embeddings_model.vectordbs['triplets'].item_exist(triplet_id)
+
+@pytest.mark.parametrize("base_triplets, triplets_to_delete, delete_nodes_flag, expected", EM_DELETE_TESTS)
+def test_delete_triplets(triplets. expected, embeddings_model):
+    embeddings_model.vectordbs['nodes'].clear()
+    embeddings_model.vectordbs['triplets'].clear()
+
+    embeddings_model.create_triplets(base_triplets, create_nodes=True)
+    deleted_item_ids = embeddings_model.delete_triplets(triplets_to_delete, delete_nodes=delete_nodes_flag)
+
+    real_nodes_db_size = embeddings_model.vectordbs['nodes'].count_items()
+    real_triplets_db_size = embeddings_model.vectordbs['triplets'].count_items()
+    assert real_nodes_db_size == expected['nodes_count']
+    assert real_triplets_db_size == expected['triplets_count']
+
+    assert len(deleted_item_ids['nodes']) == expected['deleted_n_count']
+    assert len(deleted_item_ids['triplets']) == len(deleted_item_ids['deleted_t_count'])
+
+    if add_nodes_flag:
+        for node_id in deleted_item_ids['nodes']:
+            assert not embeddings_model.vectordbs['nodes'].item_exist(node_id)
+    for triplet_id in deleted_item_ids['triplets']:
+        assert not embeddings_model.vectordbs['triplets'].item_exist(triplet_id)
--- tests/integrational/kg_model/test_graph_model.py
+++ tests/integrational/kg_model/test_graph_model.py
@@ -4,8 +4,16 @@ from cases import GM_CREATE_TESTS, GM_DELETE_TESTS
 
 @pytest.mark.parametrize("triplets, expected", GM_CREATE_TESTS)
 def test_create_triplets(triplets, expected, graph_model):
-    # TODO
-    pass
+    graph_model.db_conn.clear()
+    created_item_ids = graph_model.create_triplets(triplets)
+    assert created_item_ids['nodes'] == expected['nodes_count']
+    assert created_item_ids['triplets'] == expected['triplets_count']
+
+    for node_id in created_item_ids['nodes']:
+        assert item_exist(node_id, id_type='node')
+    for triplet_id in created_item_ids['triplets']:
+        assert item_exist(triplet_id, id_type='triplet')
+
 
 @pytest.mark.parametrize("triplet_ids, expected", GM_DELETE_TESTS)
 def test_delete_triplets(graph_model):
