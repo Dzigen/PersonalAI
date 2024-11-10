f5fc74ae8e29cc4d926bc00ff8f6d72e3629b76d
dizgen <dzigen@gmail.com>
2024-11-11 01:46:03 +0300

    update fixtures kg-model

--- tests/integrational/kg_model/conftest.py
+++ tests/integrational/kg_model/conftest.py
@@ -7,21 +7,32 @@ from src.db_drivers.graph_driver import GraphDriver, GraphDriverConfig, DEFAULT_
 from src.utils.data_structs import Triplet, TripletCreator, NodeCreator
 from src.utils import Logger
 
-# TODO
 @pytest.fixture
-def embeddings_model(db_vendor: str):
+def embeddings_chroma_model():
     config = EmbeddingsModelConfig(
         nodesdb_driver_config=VectorDriverConfig(db_config=VectorDBConnectionConfig(
-            path='./tmp/nodes', db_name='vectorized_nodes', need_to_clear=True)),
+            path='./volumes/chroma', db_info={'db': 'testing', 'table': 'vectorized_nodes'}, need_to_clear=True)),
         tripletsdb_driver_config=VectorDriverConfig(db_config=VectorDBConnectionConfig(
-            path='./tmp_triplets', db_name='vectorized_triplets', need_to_clear=True)),
+            path='./volumes/chroma', db_info={'db': 'testing', 'table': 'vectorized_triplets'}, need_to_clear=True)),
         embedder_config=EmbedderModelConfig(model_name_or_path='../../models/intfloat/multilingual-e5-small', device='cuda'))
     return EmbeddingsModel(config)
 
-
-# TODO
 @pytest.fixture
-def graph_model(db_vendor: str):
+def graph_neo4j_model():
     config = GraphModelConfig(
-        driver_config=
-    )
+        driver_config=GraphDriverConfig(
+            db_vendor='neo4j',
+            db_config=GraphDBConnectionConfig(
+                uri="bolt://localhost:7687", db_info={'db': 'testing', 'table': 'testing'}
+                params={'user': "neo4j", 'pwd': 'password'}, need_to_clear=True)))
+    return GraphModel(config)
+
+@pytest.fixture
+def graph_inmemory_model():
+    config = GraphModelConfig(
+        driver_config=GraphDriverConfig(
+            db_vendor='inmemory',
+            db_config=GraphDBConnectionConfig(
+                uri="", db_info={'db': 'testing', 'table': 'testing'}
+                params={}, need_to_clear=True)))
+    return GraphModel(config)
