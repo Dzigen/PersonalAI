9d5c615e88a1a94440b766441bdd86822410df10
dizgen <dzigen@gmail.com>
2024-11-11 02:38:19 +0300

    check if key exist before deletion

--- src/db_drivers/kv_driver/connectors/InMemoryKVConnector.py
+++ src/db_drivers/kv_driver/connectors/InMemoryKVConnector.py
@@ -62,7 +62,7 @@ class InMemoryKVConnector(AbstractKVDatabaseConnection):
 
     def delete(self, ids: List[str]):
         for id in ids:
-            if id in self.kv_store:
+            if self.item_exist(id):
                 del self.kv_store[id]
 
     def clear(self):
