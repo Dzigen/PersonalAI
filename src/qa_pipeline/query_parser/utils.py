4976e260fbd20cce155e9eba870ea4d2429d1063
Dzigen <60009703+Dzigen@users.noreply.github.com>
2024-11-12 03:02:56 +0300

    Update utils.py
    Add comments

--- src/qa_pipeline/query_parser/utils.py
+++ src/qa_pipeline/query_parser/utils.py
@@ -7,19 +7,16 @@ from ...parsers.query_parser import qa_custom_entities_parse_func
 
 QP_LOG_PATH = 'log/qp'
 
-#
 ENTITIES_EXTRACTION_USER_PROMPT = {
     'ru': ENTITIES_EXTRACTION_RU_USER_PROMPT,
     'en': ENTITIES_EXTRACTION_EN_USER_PROMPT
 }
 
-#
 ENTITIES_EXTRACTION_SYSTEM_PROMPT = {
     'ru': RU_SYSTEM_PROMPT,
     'en': EN_SYSTEM_PROMPT
 }
 
-#
 ENTITIES_PARSE_FUNC = {
     'ru': qa_custom_entities_parse_func,
     'en': qa_custom_entities_parse_func
@@ -27,9 +24,8 @@ ENTITIES_PARSE_FUNC = {
 
 @dataclass
 class EntitiesExtractorConfig:
-    #
+    # Промты для LLM-агента с описанием задачи по извлечению ключевых сущностей из текста.
     user_prompt: Dict = field(default_factory=lambda: ENTITIES_EXTRACTION_USER_PROMPT)
-    #
     system_prompt: Dict = field(default_factory=lambda: ENTITIES_EXTRACTION_SYSTEM_PROMPT)
-    #
+    # Функция парснга результатов генерации LLM-агента
     entities_parse_func: Dict = field(default_factory=lambda: ENTITIES_PARSE_FUNC)
