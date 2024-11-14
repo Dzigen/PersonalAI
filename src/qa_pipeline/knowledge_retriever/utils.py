ae016a4862734f989006eec073e97c4db775df9f
Dzigen <60009703+Dzigen@users.noreply.github.com>
2024-11-14 17:20:38 +0300

    Update utils.py

--- src/qa_pipeline/knowledge_retriever/utils.py
+++ src/qa_pipeline/knowledge_retriever/utils.py
@@ -8,28 +8,30 @@ from ...utils.data_structs import QueryInfo, Triplet
 RETRIEVER_LOG_PATH = 'log/retriever'
 
 class AbstractTriplesFilter(ABC):
+    "Интерфейс для алгоритмов фильтрации/ранжирования триплетов.
     @abstractmethod
     def apply_filter(self, query_info: QueryInfo, triplets: List[Triplet]) -> List[Triplet]:
-        """_summary_
+        """Метод предназначен для применения операциии ранжирования/фильтрации к набору триплетов на основе метрики релевантности к user-вопросу.
 
-        :param query_info: _description_
+        :param query_info: Структура данных с user-вопросом.
         :type query_info: QueryInfo
-        :param triplets: _description_
+        :param triplets: Набор триплетов для ранжирования/отбора.
         :type triplets: List[Triplet]
-        :return: _description_
+        :return: Набор триплетов, релевантных данному user-вопрос.
         :rtype: List[Triplet]
         """
         # фильтрация триплетов по заданному правилу
         pass
 
 class AbstractTripletsRetriever(ABC):
+    """Базовый класс для алгоритмов извлечения триплетов из графа знаний"""
     @abstractmethod
     def get_relevant_triplets(self, query_info: QueryInfo) -> List[Triplet]:
-        """_summary_
+        """Метод предназначен для извлечения триплетов из графа знаний на основании инфлрмации из user-вопроса. На выхрде список триплетов не содержит дубликатов (по строковому представлению).
 
-        :param query_info: _description_
+        :param query_info: Структура данных с информацией о user-вопросе
         :type query_info: QueryInfo
-        :return: _description_
+        :return: Набор триплетов, извлечённый из графа знаний.
         :rtype: List[Triplet]
         """
         # извлечение триплетов из графа знаний, релевантных запросу
@@ -37,8 +39,10 @@ class AbstractTripletsRetriever(ABC):
 
 @dataclass
 class BaseGraphSearchConfig:
+    "Базовая конфигурация для алгоритиов по извлечению триплетов из графа знаний."
     pass
 
 @dataclass
 class BaseTripletsFilterConfig:
+    "Базовая конфигурация для алгоритмоа по ранжированию/фильтрации триплетов
     pass
