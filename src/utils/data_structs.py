f2cd07f922fbad52cfdefa6a2d42da79e59e958d
dizgen <dzigen@gmail.com>
2024-11-19 12:12:42 +0300

    quick fix

--- src/utils/data_structs.py
+++ src/utils/data_structs.py
@@ -250,7 +250,7 @@ class TripletCreator(BaseCreator):
         * в случае "relation" это "simple", "hyper" и "episodic".
 
         В случае если по ключу "type" компоненты триплета "relation" указывается значение "hyper" или "episodic", то значение по соответствующему ключу "name"
-        не указывается.
+        можно не заполнять/указывать: оно использоваться не будет.
 
         Примеры json-триплетов:
         1. {'subject': {'name': 'qwe', 'type': 'object', 'prop': {'k1': 'v1'}},
