8ff1f1ff10a42640734965759b29946bb9448336
dizgen <dzigen@gmail.com>
2024-11-20 03:56:44 +0300

    quick fix

--- configs/venv_setup.sh
+++ configs/venv_setup.sh
@@ -1,11 +1,13 @@
 apt-get update
 apt-get upgrade
+echo "=== REFRESHING END ==="
 
 apt install python3.10-venv
 apt install libicu-dev python3-icu pkg-config
 
 python3.10 -m venv ../../.pai_venv
 source ../../.pai_venv/bin/activate
+echo "=== ENV-CREATION END ==="
 
 pip install pysqlite3-binary chromadb aerospike kuzu pymongo neo4j
 pip install dataclasses tqdm joblib
@@ -13,3 +15,5 @@ pip install pyicu pycld2 morfessor polyglot
 pip install numpy pandas
 pip install torch==2.4.1 transformers==4.40.0 sentence_transformers
 pip install gigachat openai
+pip install pytest
+echo "=== PACKAGES-INSTALLATION END"
