8abb18f0f2457bb394aafad8853994ef31f3c95a
dizgen <dzigen@gmail.com>
2024-11-24 22:29:07 +0300

    update parsers

--- src/parsers/memorize_pipeline/updator/replace_simple_triplets.py
+++ src/parsers/memorize_pipeline/updator/replace_simple_triplets.py
@@ -3,8 +3,20 @@ from typing import List, Dict
 from ....utils import Triplet
 
 def en_rs_parse(raw_agent_answer: str, **kwargs) -> object:
-    # TODO
-    pass
+    raw_replacements = raw_agent_answer.lower()
+    raw_replacements = raw_replacements.split("[[")[-1] if "[[" in raw_replacements else raw_replacements.split("[\n[")[-1]
+    pairs = raw_replacements.replace("[", "").strip("]").split("],")
+    triplets_to_remove = []
+    for pair in pairs:
+        splitted_pair = pair.split("->")
+        if len(splitted_pair) != 2:
+            continue
+        first_triplet = splitted_pair[0].split(",")
+        if len(first_triplet) != 3:
+            continue
+        subj, rel, obj = first_triplet[0].strip(''' \n'".,/'''), first_triplet[1].strip(''' \n'".,/'''), first_triplet[2].strip(''' \n'".,/''')
+
+        # TODO
 
 def en_rs_postprocess(foramted_agent_answer: object, base_triplet: Triplet, incident_triplets: List[Triplet]) -> List[str]:
     # TODO
@@ -19,44 +31,11 @@ def ru_rs_postprocess(foramted_agent_answer: object, base_triplet: Triplet, inci
     pass
 
 def rs_formate(base_triplet: Triplet, incident_triplets: List[Triplet]) -> Dict[str,str]:
-    # TODO
-    pass
 
-@staticmethod
-    def parse_replacements_thesis(raw_replacements):
-        raw_replacements = raw_replacements.lower()
-        predicted_outdated = raw_replacements.split("[")[-1].split("]")[0].split(";")
-        predicted_outdated = [pair.strip().split("<-")[1].strip(''' \n'".,/''') for pair in predicted_outdated if "<-" in pair]
-        triplets_to_remove = []
-        for outdated_thesis in predicted_outdated:
-            triplets_to_remove.append(
-                [
-                    {"name": "remove", "type": "remove", "prop": {}},
-                    {"name": "hyper", "prop": {"type": "hyper"}},
-                    {"name": outdated_thesis, "type": "hyper", "prop": {}}
-                ]
-            )
-        return triplets_to_remove
+    def _custom_triplet_stringify(triplet: Triplet) -> str:
+        return f"{triplet.start_node.name}, {triplet.relation.name}, {triplet.end_node.name}"
 
-    @staticmethod
-    def parse_replacements_simple(raw_replacements):
-        raw_replacements = raw_replacements.lower()
-        raw_replacements = raw_replacements.split("[[")[-1] if "[[" in raw_replacements else raw_replacements.split("[\n[")[-1]
-        pairs = raw_replacements.replace("[", "").strip("]").split("],")
-        triplets_to_remove = []
-        for pair in pairs:
-            splitted_pair = pair.split("->")
-            if len(splitted_pair) != 2:
-                continue
-            first_triplet = splitted_pair[0].split(",")
-            if len(first_triplet) != 3:
-                continue
-            subj, rel, obj = first_triplet[0].strip(''' \n'".,/'''), first_triplet[1].strip(''' \n'".,/'''), first_triplet[2].strip(''' \n'".,/''')
-            triplets_to_remove.append(
-                [
-                    {"name": subj, "type": "remove", "prop": {}},
-                    {"name": rel, "prop": {"type": "remove"}},
-                    {"name": obj, "type": "remove", "prop": {}}
-                ]
-            )
-        return triplets_to_remove
+    existing_str_triplet = f'"{_custom_triplet_stringify(base_triplet)}".'
+    new_str_triplets = '; '.join(map(lambda triplet: f'"{_custom_triplet_stringify(triplet)}"', incident_triplets)) + '.'
+
+    return {'ex_triplets': existing_str_triplet, 'new_triplets': new_str_triplets}.
--- src/parsers/memorize_pipeline/updator/replace_thesis_triplets.py
+++ src/parsers/memorize_pipeline/updator/replace_thesis_triplets.py
@@ -1,5 +1,15 @@
+from typing import List, Dict
+
+from ....utils import Triplet
+
 def en_rt_parse():
-    pass
+    raw_replacements = raw_replacements.lower()
+    predicted_outdated = raw_replacements.split("[")[-1].split("]")[0].split(";")
+    predicted_outdated = [pair.strip().split("<-")[1].strip(''' \n'".,/''') for pair in predicted_outdated if "<-" in pair]
+    triplets_to_remove = []
+    for outdated_thesis in predicted_outdated:
+        pass
+        # TODO
 
 def en_rt_postprocess():
     pass
@@ -10,5 +20,12 @@ def ru_rt_parse():
 def ru_rt_postprocess():
     pass
 
-def rt_formate():
-    pass
+def rt_formate(base_triplet: Triplet, incident_triplets: List[Triplet]) -> Dict[str,str]:
+
+    def _custom_thesis_stringify(triplet: Triplet) -> str:
+        return triplet.end_node.name
+
+    existing_str_thesise = f'["{_custom_thesis_stringify(base_triplet)}"]'
+    new_str_thesises = '[' +', '.join(map(lambda triplet: f'"{_custom_thesis_stringify(triplet)}"', incident_triplets)) + ']'
+
+    return {'ex_thesises': existing_str_thesise, 'new_thesises': new_str_thesises}
