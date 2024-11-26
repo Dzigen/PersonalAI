b8e780272d1491b83bf3b23fa8293fb01c5f48fd
dizgen <dzigen@gmail.com>
2024-11-26 23:51:29 +0300

    quick fix

--- src/agents/connectors/StubAgentConnector.py
+++ src/agents/connectors/StubAgentConnector.py
@@ -1,13 +1,14 @@
 from collections import deque
+from typing import List
 
 from ..utils import AbstractAgentConnector, AgentConnectorConfig
 
 DEFAULT_STUBAGENT_CONFIG = AgentConnectorConfig()
 
 class StubAgentConnector(AbstractAgentConnector):
-    def __init__(self, config: AgentConnectorConfig = DEFAULT_STUBAGENT_CONFIG) -> None:
+    def __init__(self, config: AgentConnectorConfig = DEFAULT_STUBAGENT_CONFIG, stub_answers: List[str] = list()) -> None:
         self.config = config
-        self.looped_answers = deque()
+        self.looped_answers = deque(stub_answers)
 
     def check_connection(self) -> bool:
         return True
