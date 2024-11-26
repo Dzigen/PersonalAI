f6dced0b2826e26b2e008798ae3022da4f7888e6
dizgen <dzigen@gmail.com>
2024-11-26 14:11:14 +0300

    update fix bug in replace_simple-parser

--- src/parsers/memorize_pipeline/updator/replace_simple_triplets.py
+++ src/parsers/memorize_pipeline/updator/replace_simple_triplets.py
@@ -29,16 +29,14 @@ def rs_custom_parse(raw_response: str, **kwargs) -> Dict[str, object]:
         existing_triplet = splitted_pair[0].split(",")
         if len(existing_triplet) != 3:
             continue
-        str_existing_triplet = existing_triplet.strip(''' \n'".,/''')
+        str_existing_triplet = splitted_pair[0].strip(''' \n'".,/''')
 
-        new_triplet = splitted_pair[0].split(",")
+        new_triplet = splitted_pair[1].split(",")
         if len(new_triplet) != 3:
             continue
-        str_new_triplet = new_triplet.strip(''' \n'".,/''')
+        str_new_triplet = splitted_pair[1].strip(''' \n'".,/''')
 
         triplets_to_remove[create_id(str_new_triplet)].add(create_id(str_existing_triplet))
-
-    return triplets_to_remove
 
 def rs_custom_postprocess(parsed_response: Dict[str, Set[str]], base_triplet: Triplet, incident_triplets: List[Triplet]) -> List[str]:
     if len(incident_triplets) < 1:
