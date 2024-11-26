005a30906ac2461e6246a236dae995cbbac43963
dizgen <dzigen@gmail.com>
2024-11-26 14:39:30 +0300

    update kwe-parse struct

--- tests/unit/parsers/test_kw_extraction.py
+++ tests/unit/parsers/test_kw_extraction.py
@@ -1,26 +1,34 @@
 import pytest
+from typing import Dict, List
 
 import sys
 sys.path.insert(0, "../../")
 from src.utils import ReturnStatus
-from src.parsers.query_parser import qa_custom_entities_parse_func
+from src.parsers.qa_pipeline.query_parser.kw_extraction import kwe_custom_formate, kwe_custom_parse, kwe_custom_postprocess
+
 
 GOOD_RESPONSE = "a | b | d"
 BAD_RESPONSE = ... # TODO
 
-@pytest.mark.parametrize("raw_response, expected_triplets, expected_status", [
+@pytest.mark.parametrize("query, expected_output, exception", [
+    ("simple query", {'text': 'simple query'}, False)
+])
+def test_custom_formate(query: str, expected_output: Dict[str, str], exception: bool):
+    try:
+         real_output = kwe_custom_formate(query)
+    except Exception:
+        assert exception
+    else:
+        assert not exception
+
+    if not exception:
+        assert expected_output == real_output
+
+@pytest.mark.parametrize("raw_response, parsed_output, exception", [
     (GOOD_RESPONSE, ['a', 'b', 'd'], ReturnStatus.success)
 ])
-def test_entities_parse_func(raw_response, expected_triplets, expected_status):
-    triplets, status = qa_custom_entities_parse_func(raw_response)
-    assert status == expected_status
-    assert triplets == expected_triplets
-
-def test_kw_formate():
+def test_kw_parse(raw_response: str, parsed_output: List[str], exception: bool):
     pass
 
-def test_kw_parse():
-    pass
-
-def test_kw_preprocess():
+def test_kw_preprocess(parsed_output: List[str], expected_output: List[str], exception: bool):
     pass
