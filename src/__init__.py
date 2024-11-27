d1ae5b2b5f8d42d2730c1128022dc76cba5f10cd
dizgen <dzigen@gmail.com>
2024-11-27 15:41:14 +0300

    quick fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .personalai_main import PersonalAI, PersonalAIConfig
 
-from .knowledge_graph_model import EmbedderModelConfig, EmbeddingsModelConfig, GraphModelConfig
+from .kg_model import EmbedderModelConfig, EmbeddingsModelConfig, GraphModelConfig
 
 from .pipelines.qa import QAPipelineConfig
 from .pipelines.memorize import MemPipelineConfig
