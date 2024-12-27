732c749f6630b1694f93a642baee0b8b917cacc4
Mikhail Menschikov <m.menschikov@megatron.appliedai.local>
2024-12-27 17:56:03 +0300

    refactor structure

--- notebooks/download_models/download_llama3_8b.py
+++ notebooks/download_models/download_llama3_8b.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 MODEL_NAME = 'Undi95/Meta-Llama-3-8B-Instruct-hf'
-SAVE_PATH = '../../models/Undi95/Meta-Llama-3-8B-Instruct-hf'
+SAVE_PATH = '../models/Undi95/Meta-Llama-3-8B-Instruct-hf'
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 model = AutoModelForCausalLM.from_pretrained(
