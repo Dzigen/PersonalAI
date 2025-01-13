bcc54877c2883d4c6dd08b2e349b26566603006d
dizgen <dzigen@gmail.com>
2025-01-13 08:10:57 +0300

    quick fix

--- src/db_drivers/vector_driver/utils.py
+++ src/db_drivers/vector_driver/utils.py
@@ -20,16 +20,5 @@ class AbstractVectorDatabaseConnection(AbstractDatabaseConnection):
     @abstractmethod
     def retrieve(self, queries: List[VectorDBInstance], n_results: int = 50,
                  includes: List[str] = ['embeddings', 'documents', 'metadatas'], **kwargs) -> List[List[Tuple[float, VectorDBInstance]]]:
-        """_summary_
-
-        :param queries: _description_
-        :type queries: List[VectorDBInstance]
-        :param n_results: _description_, defaults to 50
-        :type n_results: int, optional
-        :param includes: _description_, defaults to ['embeddings', 'documents', 'metadatas']
-        :type includes: List[str], optional
-        :return: _description_
-        :rtype: List[List[Tuple[float, VectorDBInstance]]]
-        """
         # извлечение N ближайших сущностей к данной по заданной метрике
         pass
