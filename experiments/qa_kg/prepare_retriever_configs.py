6997fb3f6c486164dc0818cd034660c6445bbcca
dizgen <dzigen@gmail.com>
2025-01-18 14:59:17 +0300

    minor naive_bfs-fix

--- experiments/qa_kg/prepare_retriever_configs.py
+++ experiments/qa_kg/prepare_retriever_configs.py
@@ -5,7 +5,8 @@ import joblib
 BASEDIR = "../../"
 sys.path.insert(0, BASEDIR)
 
-from src.pipelines.qa.knowledge_retriever import AStarGraphSearchConfig, AStarMetricsConfig, BFSSearchConfig, MixturedGraphSearchConfig, NaiveBFSGraphSearchConfig
+from src.pipelines.qa.knowledge_retriever import AStarGraphSearchConfig, AStarMetricsConfig, BFSSearchConfig,\
+    MixturedGraphSearchConfig, NaiveBFSGraphSearchConfig
 from src.pipelines.qa.knowledge_retriever.TripletsFilter import TripletsFilterConfig
 from src.db_drivers.kv_driver import KVDBConnectionConfig, KeyValueDriverConfig
 from src.db_drivers.kv_driver.connectors import DEFAULT_MIXEDKV_CONFIG
@@ -19,13 +20,13 @@ RETRIVER_CONFIG_DUMP = "retriever_config"
 
 # KV_STORAGE_CONFIG = KeyValueDriverConfig(db_vendor='mixed_kv', db_config=DEFAULT_MIXEDKV_CONFIG)
 
-# retriever_config= AStarGraphSearchConfig(
+# astar_config= AStarGraphSearchConfig(
 #     metrics_config=AStarMetricsConfig(h_metric_name='ip', kvdriver_config=KV_STORAGE_CONFIG),
 #     max_depth=8, max_passed_nodes=500,
 #     accepted_node_types=[NodeType.object , NodeType.hyper, NodeType.episodic]
 # )
 
-retriever_config = BFSSearchConfig(
+bfs_config = BFSSearchConfig(
       strict_filter = True,
       hyper_num = 15,
       episodic_num = 15,
@@ -33,9 +34,17 @@ retriever_config = BFSSearchConfig(
       other_triplets_num = 6
   )
 
-# retriever_config = NaiveBFSGraphSearchConfig(
-#     max_depth=5, max_width=10, max_passed_nodes=50,
-#     accepted_node_types=[NodeType.object , NodeType.hyper, NodeType.episodic])
+naive_bfs_config = NaiveBFSGraphSearchConfig(
+    max_depth=5, max_width=10, max_passed_nodes=50,
+    accepted_node_types=[NodeType.object , NodeType.hyper, NodeType.episodic])
+
+
+retriever_config = MixturedGraphSearchConfig(
+    retriever1_name='bfs',
+    retriever1_config=bfs_config,
+    retriever2_name='naive_bfs',
+    retriever2_config=naive_bfs_config
+)
 
 joblib.dump(retriever_config, RETRIVER_CONFIG_DUMP)
 
