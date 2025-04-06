1ee1963c0ef9e8b03116e928ff420dd8efb6bb18
dizgen <dzigen@gmail.com>
2025-04-06 20:39:40 +0300

    update default agent-connectors hyperparams

--- src/agents/connectors/OpenAIConnector.py
+++ src/agents/connectors/OpenAIConnector.py
@@ -7,12 +7,14 @@ from ..utils import AbstractAgentConnector, AgentConnectorConfig
 DEEPSEEK_KEY = 'sk-7114ae174a6142bf8b028e8bf6af9579'
 DEEPSEEK_CONFIG = AgentConnectorConfig(
     gen_strategy={'max_tokens': 4096, 'seed': 42, 'top_p': 10e-16, 'temperature': 0.0, 'frequency_penalty':0, 'presence_penalty':0},
-    credentials={'token': DEEPSEEK_KEY, 'model': 'deepseek-chat', 'base_url': 'https://api.deepseek.com'})
+    credentials={'token': DEEPSEEK_KEY, 'model': 'deepseek-chat', 'base_url': 'https://api.deepseek.com'},
+    ext_params={'timeout': 560, 'max_retries': 5})
 
 GPT4OMINI_KEY = "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"
 GPT4OMINI_CONFIG = AgentConnectorConfig(
     gen_strategy={'max_tokens': 4096, 'seed': 42, 'top_p': 10e-16, 'temperature': 0.0, 'frequency_penalty':0, 'presence_penalty':0},
-    credentials={'token': GPT4OMINI_KEY, 'model': 'gpt-4o-mini', 'base_url': None})
+    credentials={'token': GPT4OMINI_KEY, 'model': 'gpt-4o-mini', 'base_url': None},
+    ext_params={'timeout': 560, 'max_retries': 5})
 
 
 class OpenAIConnector(AbstractAgentConnector):
