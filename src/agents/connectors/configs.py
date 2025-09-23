03c618187613a88f45ec08c4980994798aec0a8f
dzigen <dzigen@gmail.com>
2025-09-23 13:14:24 +0300

    move secrets to env

--- src/agents/connectors/configs.py
+++ src/agents/connectors/configs.py
@@ -1,8 +1,12 @@
 import torch
+from dotenv import load_dotenv
+import os
 
 from ..utils import AgentConnectorConfig
 
-GIGACHAT_KEY = 'OWUwOGUzOWEtMjJiNi00YmMxLThmMmItNzMwNjM2MTI2YmYxOjg2ODdiOTVhLTZkNDctNGFjOC1iMmViLTEyNDA5MmFiN2Q5Mw=='
+load_dotenv(".env_secret_llmkeys")
+
+GIGACHAT_KEY = os.getenv("GIGACHAT_KEY")
 DEFAULT_GIGACHAT_CONFIG = AgentConnectorConfig(
     gen_strategy={'top_k': 1, 'top_p': 0, 'temperature': 0},
     credentials={'token': GIGACHAT_KEY,
@@ -22,7 +26,7 @@ DEFAULT_OLLAMA_CONFIG = AgentConnectorConfig(
     credentials={'model': 'llama3.1:8b'},
     ext_params={'host': 'localhost', 'port': 11434, 'timeout': 560, 'keep_alive': -1})
 
-DEEPSEEK_KEY = 'sk-aa2371fe6326458bbd63bf91dcb6a4a4'
+DEEPSEEK_KEY = os.getenv("DEEPSEEK_KEY")
 DEEPSEEK_CONFIG = AgentConnectorConfig(
     gen_strategy={'seed': 42, 'top_p': 10e-16, 'temperature': 0.0,
                   'frequency_penalty': 0, 'presence_penalty': 0},
@@ -30,7 +34,7 @@ DEEPSEEK_CONFIG = AgentConnectorConfig(
                  'base_url': 'https://api.deepseek.com'},
     ext_params={'timeout': 560, 'max_retries': 5})
 
-GPT4OMINI_KEY = "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"
+GPT4OMINI_KEY = os.getenv("GPT4OMINI_KEY")
 GPT4OMINI_CONFIG = AgentConnectorConfig(
     gen_strategy={'seed': 42, 'top_p': 10e-16, 'temperature': 0.0,
                   'frequency_penalty': 0, 'presence_penalty': 0},
