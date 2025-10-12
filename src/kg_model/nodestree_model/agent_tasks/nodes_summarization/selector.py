c5e531c6431fd30257768fe037fcd9059776a107
dzigen <dzigen@gmail.com>
2025-10-13 01:43:25 +0300

    quick fix

--- src/kg_model/nodestree_model/agent_tasks/nodes_summarization/selector.py
+++ src/kg_model/nodestree_model/agent_tasks/nodes_summarization/selector.py
@@ -15,10 +15,13 @@ class AgentSummNTaskConfigSelector:
         return AVAILABLE_SUMMN_TCONFIGS
 
     @staticmethod
-    def select(base_config_version: str = 'v1', cache_table_name: str = "qa_agent_summn_task_cache") -> AgentTaskSolverConfig:
+    def select(base_config_version: str = 'v1',
+               cache_table_name: str = "qa_agent_summn_task_cache",
+               inferencestat_table_name: str = "qa_agent_summn_task_stat") -> AgentTaskSolverConfig:
         return AgentTaskSolverConfig(
             version=base_config_version,
             suites=AVAILABLE_SUMMN_TCONFIGS[base_config_version],
             formate_context_func=summn_custom_formate, postprocess_answer_func=summn_custom_postprocess,
             cache_table_name=cache_table_name,
+            inferencestat_table_name=inferencestat_table_name,
             log=Logger(SUMM_NODES_LOG_PATH))
