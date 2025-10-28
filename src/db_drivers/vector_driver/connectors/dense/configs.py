a788e0ada6a460c6774120f86afcead7d3d3f69c
Dzigen <dzigen@gmail.com>
2025-10-28 14:37:51 +0300

    init conn-files

--- src/db_drivers/vector_driver/connectors/dense/configs.py
+++ src/db_drivers/vector_driver/connectors/dense/configs.py
@@ -9,3 +9,17 @@ DEFAULT_MILVUS_CONFIG = VectorDBConnectionConfig(
           'user': 'root', 'pass': 'Milvus'},
     params={'id_length': 32, 'vector_dim': 1024, 'document_max_length': 51200, 'load': True,
             'flush': True, 'create_sleep': 1, 'search_metric': 'IP'})
+
+DEFAULT_INMEMORY_CONFIG = VectorDBConnectionConfig(
+    params={
+        'load_from_disk': False,
+        'load_dump_name': None,
+        'load_dump_dir': "./personalai_tmp/volumes/inmemory_dense",
+        'save_on_disk': True,
+        'save_dump_dir': "./personalai_tmp/volumes/inmemory_dense"
+    }
+)
+
+DEFAULT_OPENSEARCH_CONFIG = VectorDBConnectionConfig(
+    conn={'host': 'localhost', 'port': 9200, 'user': 'admin', 'pass': 'admin'}
+)
