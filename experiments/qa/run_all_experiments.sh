a2552f08ba7d6139942baf725f74fd7af1881e53
Dzigen <dzigen@gmail.com>
2025-11-08 11:02:04 +0300

    update scripts for qa experiments

--- experiments/qa/run_all_experiments.sh
+++ experiments/qa/run_all_experiments.sh
@@ -1,39 +1,46 @@
 #!/usr/bin/bash
+# Запуск набора экспериментов
 
+EXP_BASE_DIR=/home/workspace/experiments/qa
+PYTHON_CMD=/usr/bin/python3
+PREPARED_PARAMS_NAME=prepared_params
 
-# BASE_DIR=/home/workspace/experiments
-# 14 23 16 05 * bash run_all_experiments.sh >> runall_cron.txt 2>&!
+# --------------------------------------------------------
 
-declare -A exp_names
+RUNEXP_LOG_PATH="$EXP_BASE_DIR/runexperiment_log.txt"
+CONFIGURE_BASE_DIR="$EXP_BASE_DIR/configure"
+EVALUATE_BASE_DIR="$EXP_BASE_DIR/evaluate"
+
+# --------------------------------------------------------
+
+KNOWLEDGEGRAPH_NAME=deepseek_231025_v2prompts # TO CHANGE
+DATASET_NAME=hotpotqa_distractor_validation # TO CHANGE
+QAPIPELINE_KW="weak" # TO CHANGE
+
+# EXP_NAME
+declare -A exp_names # TO CHANGE
 exp_names[0]="m_v2_gpt4omini(e4.4.1)(v1.5.5)"
 exp_names[1]="m_v2_gpt4omini(e4.4.2)(v1.5.5)"
-exp_names[2]="m_v2_gpt4omini(e4.4.3)(v1.5.5)"
-exp_names[3]="m_v2_gpt4omini(e4.4.4)(v1.5.5)"
-exp_names[4]="m_v2_gpt4omini(e4.5.2)(v1.5.5)"
-exp_names[5]="m_v2_gpt4omini(e4.5.3)(v1.5.5)"
-exp_names[6]="m_v2_gpt4omini(e4.5.4)(v1.5.5)"
-exp_names[7]="m_v2_gpt4omini(e4.6.2)(v1.5.5)"
-exp_names[8]="m_v2_gpt4omini(e4.6.3)(v1.5.5)"
-exp_names[9]="m_v2_gpt4omini(e4.6.4)(v1.5.5)"
 
-declare -A param_names
-param_names[0]="e4.4.1.yaml"
-param_names[1]="e4.4.2.yaml"
-param_names[2]="e4.4.3.yaml"
-param_names[3]="e4.4.4.yaml"
-param_names[4]="e4.5.2.yaml"
-param_names[5]="e4.5.3.yaml"
-param_names[6]="e4.5.4.yaml"
-param_names[7]="e4.6.2.yaml"
-param_names[8]="e4.6.3.yaml"
-param_names[9]="e4.6.4.yaml"
+# QAHYPERP_PARAMS_PATH
+declare -A exp_hyperp # TO CHANGE
+exp_hyperp[0]="m_v2_gpt4omini(e4.4.1)(v1.5.5)"
+exp_hyperp[1]="m_v2_gpt4omini(e4.4.2)(v1.5.5)"
+
+EXPDIR_PARAMS_PATH="$CONFIGURE_BASE_DIR/$PREPARED_PARAMS_NAME/hotpotqa_distractor_validation.yaml" # TO CHANGE
+
+EVAL_PARAMS_PATH="$EVALUATE_BASE_DIR/eval_params.yaml" # TO CHANGE
+
+# --------------------------------------------------------
 
 for exp_idx in "${!exp_names[@]}";
 do
-    CRONTAB_DIR=/home/workspace/experiments
-    DATASET_NAME=trivia_qa_rcwikipedia_validation
-    MODEL_NAME=llama318b
-    echo "${param_names[$exp_idx]} | ${exp_names[$exp_idx]}"
+    echo "$KNOWLEDGEGRAPH_NAME | $DATASET_NAME | ${exp_names[$exp_idx]}"
+    echo "qahyperp params-path: ${exp_hyperp[$exp_idx]}"
+    echo "expdir params-path: $EXPDIR_PARAMS_PATH"
+    echo "eval params-path: $EVAL_PARAMS_PATH"
 
-    bash $CRONTAB_DIR/run_experiment.sh $MODEL_NAME "${param_names[$exp_idx]}" "${exp_names[$exp_idx]}" $DATASET_NAME >> qa_log.txt 2>&1
+    bash $EXP_BASE_DIR/run_experiment.sh $KNOWLEDGEGRAPH_NAME $DATASET_NAME "${exp_names[$exp_idx]}" "${exp_hyperp[$exp_idx]}" $EXPDIR_PARAMS_PATH $EVAL_PARAMS_PATH  >> $RUNEXP_LOG_PATH 2>&1
 done
+
+echo "=== Done (run_all_experiments.sh) ==="
