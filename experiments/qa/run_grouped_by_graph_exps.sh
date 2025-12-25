c2b7187b2ed2f1bc055eaf8713c9710869435b3e
Dzigen <menshikov.mikhail.2001@gmail.com>
2025-12-25 11:06:34 +0300

    quick fix

--- experiments/qa/run_grouped_by_graph_exps.sh
+++ experiments/qa/run_grouped_by_graph_exps.sh
@@ -14,10 +14,11 @@ ENV_SETTINGS_DIR="$INIT_ENV_DIR/env_settings"
 
 # ===============================================================
 
-DATASETS=("sberdialogues_conv-21" "sberdialogues_conv-22" "sberdialogues_conv-23" "sberdialogues_conv-24" "sberdialogues_conv-25" "sberdialogues_conv-26" "sberdialogues_conv-27" "sberdialogues_conv-28" "sberdialogues_conv-29" "sberdialogues_conv-30" "sberdialogues_conv-31" "sberdialogues_conv-32" "sberdialogues_conv-33" "sberdialogues_conv-34" "sberdialogues_conv-35") # TO CHANGE
-KNOWLEDGE_GRAPHS=("gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts") # TO CHANGE
-EVAL_FNAMES=("sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml") # TO CHANGE
-CONFIGURE_FNAMES=("sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml") # TO CHANGE
+# 21 (sberdialogues_gigachatmax_medium_mixture(#f7ede21f)(v2.1.8); llm-as-a-judge) |
+DATASETS=("sberdialogues_conv-24" "sberdialogues_conv-25" "sberdialogues_conv-26" "sberdialogues_conv-27" "sberdialogues_conv-28" "sberdialogues_conv-29" "sberdialogues_conv-30" "sberdialogues_conv-31" "sberdialogues_conv-32" "sberdialogues_conv-33" "sberdialogues_conv-34" "sberdialogues_conv-35") # TO CHANGE
+KNOWLEDGE_GRAPHS=("gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts" "gigachatmax_111225_v2prompts") # TO CHANGE
+EVAL_FNAMES=("sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml") # TO CHANGE
+CONFIGURE_FNAMES=("sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml" "sberdialogues.yaml") # TO CHANGE
 
 # ===============================================================
 
