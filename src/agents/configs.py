8d3a7f4f2016fe5ad9735e4a260d72ec0c4c17a7
Dzigen <dzigen@gmail.com>
2026-07-01 02:32:34 +0300

    add configs serialization/deserialization

--- src/agents/configs.py
+++ src/agents/configs.py
@@ -1,10 +1,13 @@
+from typing import Dict
 from .connectors import LocalAgentConnector, OLlamaConnector, \
     OpenAIConnector, GigaChatConnector, StubAgentConnector
 
 from .connectors.configs import DEFAULT_GIGACHAT_CONFIG, DEFAULT_OLLAMA_CONFIG, \
     GPT4OMINI_CONFIG, DEEPSEEK_CONFIG, DEFAULT_LOCALAGENT_CONFIG, DEFAULT_STUBAGENT_CONFIG
+from .utils import AbstractAgentConnector, AgentConnectorConfig
 
-DEFAULT_AGENT_CONFIGS = {
+
+DEFAULT_AGENT_CONFIGS: Dict[str, AgentConnectorConfig] = {
     'local': DEFAULT_LOCALAGENT_CONFIG,
     'ollama': DEFAULT_OLLAMA_CONFIG,
     'gigachat': DEFAULT_GIGACHAT_CONFIG,
@@ -12,7 +15,7 @@ DEFAULT_AGENT_CONFIGS = {
     'stub': DEFAULT_STUBAGENT_CONFIG
 }
 
-AVAILABLE_AGENT_CONNECTORS = {
+AVAILABLE_AGENT_CONNECTORS: Dict[str, AbstractAgentConnector] = {
     'local': LocalAgentConnector,
     'ollama': OLlamaConnector,
     'gigachat': GigaChatConnector,
--- src/utils/agent_stat_analyzer/utils.py
+++ src/utils/agent_stat_analyzer/utils.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass
 from typing import Union
 from abc import ABC, abstractmethod
 
-from ...db_drivers.table_driver.utils import BaseTableStucture
-
 
 @dataclass
 class CalculateMetrics:
@@ -19,25 +17,6 @@ class CalculateMetrics:
     mean: bool = True
     std: bool = True
     sum: bool = True
-
-
-@dataclass
-class LLMInferenceStat(BaseTableStucture):
-    """Структура данных для хранения статистики одной inference-операции LLM.
-
-    :param prompt_tokens_amount: Количество токенов во входном промпте.
-    :type prompt_tokens_amount: int
-    :param generated_tokens_amount: Количество токенов, сгенерированных моделью.
-    :type generated_tokens_amount: int
-    :param inference_elapsed_time: Время инференса в секундах.
-    :type inference_elapsed_time: float
-    :param timestamp: Временная метка записи в БД.
-    :type timestamp: Union[None, str]
-    """
-    prompt_tokens_amount: int
-    generated_tokens_amount: int
-    inference_elapsed_time: float  # in seconds
-    timestamp: Union[None, str] = None
 
 
 class AbstractTableStatOperations(ABC):
