c38282c86de9dec8a3017d004e63e2368eb2efc0
M.Menschikov@skoltech.ru <menshikov.mikhail.2001@gmail.com>
2026-07-21 10:26:32 +0300

    handle situation, them LLM-agent return None

--- src/pipelines/qa/kg_reasoning/medium_reasoner/searchplan_enhancer/SearchPlanEnhancer.py
+++ src/pipelines/qa/kg_reasoning/medium_reasoner/searchplan_enhancer/SearchPlanEnhancer.py
@@ -132,13 +132,17 @@ class SearchPlanEnhancer(CacheUtils, CacheOperations, AgentStatOperations):
             new_search_steps, status, trace = self.tasks_solvers.plan_initialing_solver.solve(
                 lang=self.config.lang, gen_strategy=self.config.agent_gen_stategy, query=search_plan.base_query)
             module_trace.add("plan_initialing_solver", ModuleType.task_solver, trace)
-            str_searchplan = "\n".join([f'{i}. {gen_step}' for i, gen_step in enumerate(new_search_steps)])
-            self.log.debug("RESULT: %d\n%s", len(new_search_steps), str_searchplan, verbose=self.verbose, log_level=self.log_level)
-
+            
             if status == ReturnStatus.success:
+                str_searchplan = "\n".join([f'{i}. {gen_step}' for i, gen_step in enumerate(new_search_steps)])
+                self.log.debug("RESULT: %d\n%s", len(new_search_steps), str_searchplan, verbose=self.verbose, log_level=self.log_level)
+
                 enhanced_search_plan = deepcopy(search_plan)
                 enhanced_search_plan.search_steps = new_search_steps
                 enhanced_search_plan.steps_answers = []
+            else:
+                self.log.debug("RESULT: -1\n%s", str_searchplan, verbose=self.verbose, log_level=self.log_level)
+
         elif self.config.plan_enhancment:
             self.log.debug("Выполняем проверку на необходимость улучшения следующих шагов поиска в плане...", verbose=self.verbose, log_level=self.log_level)
             need_enhance, status, trace = self.tasks_solvers.enhance_classify_solver.solve(
