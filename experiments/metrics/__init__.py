2a8112d8c337dfaff6747c26181f86926a9cbc44
Dzigen <dzigen@gmail.com>
2026-08-17 17:34:27 +0300

    quick fix

--- experiments/metrics/__init__.py
+++ experiments/metrics/__init__.py
@@ -1,4 +1,4 @@
 from .llm_as_a_judge import AnswersJudge, AnswersJudgeConfig
 from .base_metrics import ReaderMetrics, calculate_f1_generation
 from .llm_as_a_judge_mine import MINEJudge, MINEJudgeConfig
-#from .my_ragas import RagasMetricsConfig, RagasMetrics
+from .my_ragas import RagasMetricsConfig, RagasMetrics
